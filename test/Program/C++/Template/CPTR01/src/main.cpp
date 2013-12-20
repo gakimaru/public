70e55d72f66baf24e38d2de0ccbcb5a7d41f2434
板垣 衛 <ita_gaki@wd5.so-net.ne.jp>
2013-12-20 16:31:02 +0900

    STLコンテナとアルゴリズムのテストを追加。

--- test/Program/C++/Template/CPTR01/src/main.cpp
+++ test/Program/C++/Template/CPTR01/src/main.cpp
@@ -2,8 +2,14 @@
 #include <stdlib.h>
 
 #include <iostream>
+#include <vector>
+#include <iterator>
+#include <functional>
+#include <algorithm>
 
 #include "lib/TemplateTest.h"
+
+#define countof(x) (sizeof(x) / sizeof(x[0]))
 
 void sub1();
 void sub2();
@@ -14,6 +20,29 @@ int main(const int argc, const char* argv[])
 
 	sub1();
 	sub2();
+
+	//おまけ：STLのアルゴリズムをテスト
+	{
+		std::cout << "- Sort normal array -" << std::endl;
+		int arr[] = { 1, 2, 3, 4, 5 };
+		std::random_shuffle(&arr[0], &arr[countof(arr)]);		     std::cout << "init:   "; std::copy(&arr[0], &arr[countof(arr)], std::ostream_iterator<int>(std::cout, " ")); std::cout << std::endl;
+		std::sort(&arr[0], &arr[countof(arr)]);		                 std::cout << "sorted1:"; std::copy(&arr[0], &arr[countof(arr)], std::ostream_iterator<int>(std::cout, " ")); std::cout << std::endl;
+		std::sort(&arr[0], &arr[countof(arr)], std::greater<int>()); std::cout << "sorted2:"; std::copy(&arr[0], &arr[countof(arr)], std::ostream_iterator<int>(std::cout, " ")); std::cout << std::endl;
+		std::sort(&arr[0], &arr[countof(arr)], std::less<int>());    std::cout << "sorted3:"; std::copy(&arr[0], &arr[countof(arr)], std::ostream_iterator<int>(std::cout, " ")); std::cout << std::endl;
+	}
+	{
+		std::cout << "- Sort std::vector -" << std::endl;
+		std::vector<int> vec;
+		vec.push_back(1);
+		vec.push_back(2);
+		vec.push_back(3);
+		vec.push_back(4);
+		vec.push_back(5);
+		std::random_shuffle(vec.begin(), vec.end());		    std::cout << "init:   "; std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " ")); std::cout << std::endl;
+		std::sort(vec.begin(), vec.end());		                std::cout << "sorted1:"; std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " ")); std::cout << std::endl;
+		std::sort(vec.begin(), vec.end(), std::greater<int>());	std::cout << "sorted2:"; std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " ")); std::cout << std::endl;
+		std::sort(vec.begin(), vec.end(), std::less<int>());	std::cout << "sorted3:"; std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " ")); std::cout << std::endl;
+	}
 
 	std::cout << "- main():END -" << std::endl;
 	
